943ab7d59495b7048d0024d4e6d80fc3321692f8
Tony Lagnada <deloyz@gmail.com>
2026-01-08 17:53:39 -0800

    Setup update

--- dev/setup.sh
+++ dev/setup.sh
@@ -4,7 +4,7 @@ ADDON_NAME=Gears
 # REL_CONFIG Must match package-as in pull-extlib.toc
 # package-as: dev/pull-extlib
 REL_CONFIG=dev/pull-extlib.yaml
-BUILD_DIR=./.setup
+BUILD_DIR=./.release
 SCRIPT_DIR=./dev
 RELEASE_SCRIPT=${SCRIPT_DIR}/release.sh
 PACKAGE_NAME=Pull-Ext-Lib
