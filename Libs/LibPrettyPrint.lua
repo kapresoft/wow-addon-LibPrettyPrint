341e78028b5b523dd67d43490ac9d79f0ec496d8
Tony Lagnada <deloyz@gmail.com>
2026-01-27 17:46:29 -0800

    Bump version. Fix for issue #3.

--- Libs/LibPrettyPrint.lua
+++ Libs/LibPrettyPrint.lua
@@ -6,7 +6,7 @@ local LibStub = LibStub
 --[[-----------------------------------------------------------------------------
 LibPrettyPrint
 -------------------------------------------------------------------------------]]
-local MAJOR, MINOR = 'LibPrettyPrint-1.0', 2
+local MAJOR, MINOR = 'LibPrettyPrint-1.0', 3
 
 --- @class LibPrettyPrint
 local S = LibStub:NewLibrary(MAJOR, MINOR); if not S then return end
